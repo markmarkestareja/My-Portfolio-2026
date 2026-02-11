e206d85b06d26941fe909f58a33dae9dac22feba
Mark Estareja <mark.estareja.me@gmail.com>
2026-02-11 21:39:01 +0800

    Added styling in about and padding in sections

--- src/pages/hero/aboutHero.jsx
+++ src/pages/hero/aboutHero.jsx
@@ -1,10 +1,20 @@
 function AboutHero() {
-    return (
-        <section className="h-lvh bg-black-200">
-            <p>about me</p>
-            <h2>CONTINUOUSLY CRAFTING CODE & CREATIVITY</h2>
-        </section>
-    )
+  return (
+    <section className="h-lvh bg-black-200">
+      <div className="w-2/3">
+        <p>about me</p>
+        <h2>CONTINUOUSLY CRAFTING CODE & CREATIVITY</h2>
+      </div>
+      <p>
+        I'm Mark Estareja, an aspiring web developer and graphic designer
+        passionate about building user-focused digital experiences. I specialize
+        in web development, UI/UX design, and graphics for online platforms and
+        businesses. With hands-on experience in both front-end and back-end
+        development, I create solutions that are not only functional but
+        visually engaging.
+      </p>
+    </section>
+  );
 }
 
 export default AboutHero;
